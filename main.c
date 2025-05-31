f43fd07207f6feac37e7ceab0f8a66e7e3f3d6d4
Shafwan Khairullah <awannshakaru@gmail.com>
2025-05-31 16:12:55 +0700

    Program C Pertama

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    // Ini Adalah Komentar!
+    /*
+    Ini Juga
+    */
+    printf("Hello World!\n");
+    printf("Ini adalah program C pertamaku!");
+
+    return 0;
+}

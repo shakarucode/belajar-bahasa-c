896ff8b8bead7badc9b7cb2b5d1fcdc580745589
Shafwan Khairullah <awannshakaru@gmail.com>
2025-05-31 19:37:05 +0700

    Variable pada Bahasa C

--- variables.c
+++ variables.c
@@ -2,5 +2,14 @@
 
 int main()
 {
+    // Variable adalah tempat yang dapat digunakan kembali untuk menyimpan sebuah nilai (value)
+    // Berperilaku seolah-olah itu nilai yang dikandungnya
+    // Masing-masing tipe data memiliki cara unik untuk membuat variabelnya, seperti int untuk angka.
+    int age = 18;
+    // %d untuk format specifiers artinya desimal
+    printf("Aku berumur %d tahun.", age);
+    int year = 2025;
+    printf("Sekarang Tahun %d", year);
+
     return 0;
 }
